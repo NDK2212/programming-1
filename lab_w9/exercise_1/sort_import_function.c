7def5df2fb16d450d45e0dd6a18e4b1ba2c50349
NDKhang2212 <nguyenduckhang0445@gmail.com>
2024-11-26 10:52:43 +0700

    Lab week 9

--- lab_w9/exercise_1/sort_import_function.c
+++ lab_w9/exercise_1/sort_import_function.c
@@ -2,16 +2,16 @@
 #include <stddef.h>
 #include <stdio.h>
 
-void* handle = NULL;
+void *handle = NULL;
 #define GetProcAddress dlsym
 
 // declare function pointers
 
-int (*greater) (int, int);
-int (*less) (int, int);
-void (*bubble_sort) (int*, int);
+int (*greater)(int, int);
+int (*less)(int, int);
+void (*bubble_sort)(int *, int, int (*)(int, int));
 
-void DllOpen(const char* lib_name)
+void DllOpen(const char *lib_name)
 {
     handle = dlopen(lib_name, RTLD_LAZY);
 
@@ -21,17 +21,25 @@ void DllOpen(const char* lib_name)
         return;
     }
 
-    greater = (int (*)(int, int)) GetProcAddress(handle, "greater");
-    less = (int (*)(int, int)) GetProcAddress(handle, "less");
-    bubble_sort = (void(*)(int*, int)) GetProcAddress(handle, "bubble_sort");
+    greater = (int (*)(int, int))GetProcAddress(handle, "greater");
+    less = (int (*)(int, int))GetProcAddress(handle, "less");
+    bubble_sort = (void (*)(int *, int, int (*)(int, int)))GetProcAddress(handle, "bubble_sort");
 }
 
 int main()
 {
     int N = 5;
-    int arr[] = { 1, 4, 3, 7, 6 };
+    int arr[] = {1, 4, 3, 7, 6};
     DllOpen("./sort_lib.so");
 
-    bubble_sort(arr, N);
+    bubble_sort(arr, N, greater);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
     return 0; // set break point
+    // gcc - c - fPIC sort_lib.c - o sort_lib.o
+    // gcc sort_lib.o - shared - o sort_lib.so
+    // cd "d:\programming-1\lab_w9\exercise_1\" ; if ($?) { gcc sort_import_function.c -o sort_import_function } ; if ($?) { .\sort_import_function }
 }
--- lab_w9/exercise_1/sort_lib.c
+++ lab_w9/exercise_1/sort_lib.c
@@ -1,29 +1,28 @@
 
 extern int greater(int a, int b);
 extern int less(int a, int b);
-extern void bubble_sort(int* arr, int N);
+extern void bubble_sort(int *arr, int N, int *cmp_func(int, int));
 
 // exchange values of the pointers
-void swap(int* a, int* b)
+void swap(int *a, int *b)
 {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void bubble_sort(int* arr, int N)
+void bubble_sort(int *arr, int N, int *cmp_func(int, int))
 {
     if (N >= 2)
     {
         for (int i = 0; i < N - 1; i++)
         {
-            if (arr[i] > arr[i + 1])
+            if (cmp_func(arr[i], arr[i + 1]))
             {
                 swap(&(arr[i]), &arr[i + 1]);
             }
         }
-
-        bubble_sort(arr, N - 1);
+        bubble_sort(arr, N - 1, cmp_func);
     }
 }
 
@@ -31,7 +30,6 @@ int greater(int a, int b)
 {
     return a > b ? 1 : 0; // ternary operator
 }
-
 
 int less(int a, int b)
 {
